28e8e52b6588a8c2651b7548db58c1fab93e29d4
ThatOnePersons417 <47368541+ThatOnePersons417@users.noreply.github.com>
2019-04-01 19:28:59 -0700

    Added all files

---
+++ Calculator/CalculatorManager.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Calculator
+{
+    class CalculatorManager
+    {
+        private List<string> easyReadEquation;
+        private Stack<char> operatorStack;
+
+        //Creates the easyReadEquation and operatorStack
+        public CalculatorManager()
+        {
+            ResetLists();
+        }
+
+        //Resets the easyReadEquation and operatorStack
+        private void ResetLists()
+        {
+            operatorStack = new Stack<char>();
+            easyReadEquation = new List<string>();
+        }
+
+        //Resets the easyReadEquation and operatorStack, simplifys the passed equation, and returns the solved equation
+        public float CalculateEquation(string equation)
+        {
+            ResetLists();
+            SimplifyEquation(equation);
+            return SolveEquation();
+        }
+
+        //Solves the equation and returns the answer
+        private float SolveEquation()
+        {
+            Stack<float> numbers = new Stack<float>();
+            for (int i = 0; i < easyReadEquation.Count; i++)
+            {
+                try
+                {
+                    float number = float.Parse(easyReadEquation[i]);
+                    numbers.Push(number);
+                } catch (System.FormatException)
+                {
+                    numbers.Push(ApplyOperator(numbers.Pop(), numbers.Pop(), easyReadEquation[i]));
+                }
+            }
+            return numbers.Pop();
+        }
+
+        //Simplifys the equation to a form that the computer does not have to consider order of operations
+        private void SimplifyEquation(string equation)
+        {
+            for (int i = 0; i < equation.Length; i++)
+            {
+                //Number here
+                if (char.IsDigit(equation[i]) || equation[i] == 'N')
+                {
+                    Tuple<string, int> number = GetNumber(equation.Replace('N', '-'), i);
+                    easyReadEquation.Add(number.Item1);
+                    i += number.Item2;
+                }//Operator or decimal point here
+                else
+                {
+                    if (equation[i] == ')')
+                    {
+                        while (operatorStack.Peek() != '(')
+                        {
+                            easyReadEquation.Add(operatorStack.Pop().ToString());
+                        }
+                        operatorStack.Pop();
+                    }
+                    else if (operatorStack.Count == 0 || GetOperatorHeiarchyLevel(equation[i]) < GetOperatorHeiarchyLevel(operatorStack.Peek()))
+                    {
+                        operatorStack.Push(equation[i]);
+                    }
+                    else
+                    {
+                        if (operatorStack.Peek() != '(')
+                        {
+                            easyReadEquation.Add(operatorStack.Pop().ToString());
+                            operatorStack.Push(equation[i]);
+                        }
+                        else
+                        {
+                            operatorStack.Push(equation[i]);
+                        }
+                    }
+                }
+            }
+            while (operatorStack.Count > 0)
+            {
+                easyReadEquation.Add(operatorStack.Pop().ToString());
+            }
+        }
+
+        private float ApplyOperator(float num2, float num1, string op)
+        {
+            switch (op)
+            {
+                case "+":
+                    return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "/":
+                    return num1 / num2;
+                case "*":
+                    return num1 * num2;
+                default:
+                    return 0F;
+            }
+        }
+        
+        //Returns the heiarchy level of the passed operator
+        private int GetOperatorHeiarchyLevel(char op)
+        {
+            switch (op)
+            {
+                case '(':
+                    return 0;
+                case ')':
+                    return 0;
+                case '*':
+                    return 1;
+                case '/':
+                    return 1;
+                case '+':
+                    return 2;
+                case '-':
+                    return 2;
+                default:
+                    return 0;
+            }
+        }
+
+        //Returns a tuple containing the number found at the current index and the length of the number
+        private Tuple<string, int> GetNumber(string equation, int currentIndex, int _offset = 0)
+        {
+            string number = "";
+            number += equation[currentIndex];
+            if (currentIndex + 1 < equation.Length && (Char.IsDigit(equation[currentIndex + 1]) || equation[currentIndex + 1] == '.'))
+            {
+                Tuple<string, int> result = GetNumber(equation, currentIndex + 1, _offset);
+                number += result.Item1;
+                _offset = result.Item2 + 1;
+            }
+            Tuple<string, int> output = new Tuple<string, int>(number, _offset);
+            return output;
+        }
+    }
+}
---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CalculatorManager calculator;
+        private string actualEquation = "";
+        private string viewEquation = "";
+        private bool dotClicked = false;
+        private bool resultCalculated = false;
+        bool negativeClicked = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            calculator = new CalculatorManager();
+        }
+
+        private void NumberClicked(int number)
+        {
+            UpdateEquation(number.ToString());
+        }
+
+        private void OperatorClicked(string op)
+        {
+            dotClicked = false;
+            negativeClicked = false;
+            UpdateEquation(op);
+        }
+
+        private void UpdateEquation(string chr)
+        {
+            if (resultCalculated == true)
+            {
+                Clear();
+                resultCalculated = false;
+            }
+            if (DisplayText.ActualWidth < 350)
+            {
+                actualEquation += chr;
+                viewEquation += chr;
+            }
+        }
+
+        private void Clear()
+        {
+            actualEquation = "";
+            viewEquation = "";
+            ResulText.Text = "";
+            dotClicked = false;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            DisplayText.Text = viewEquation;
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private void Nine_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(9);
+            UpdateDisplay();
+        }
+
+        private void Eight_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(8);
+            UpdateDisplay();
+        }
+
+        private void Seven_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(7);
+            UpdateDisplay();
+        }
+
+        private void Six_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(6);
+            UpdateDisplay();
+        }
+
+        private void Five_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(5);
+            UpdateDisplay();
+        }
+
+        private void Four_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(4);
+            UpdateDisplay();
+        }
+
+        private void Three_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(3);
+            UpdateDisplay();
+        }
+
+        private void Two_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(2);
+            UpdateDisplay();
+        }
+
+        private void One_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(1);
+            UpdateDisplay();
+        }
+
+        private void Zero_Click(object sender, RoutedEventArgs e)
+        {
+            NumberClicked(0);
+            UpdateDisplay();
+        }
+
+        private void Negative_Click(object sender, RoutedEventArgs e)
+        {
+            if (!negativeClicked)
+            {
+                actualEquation += "N";
+                viewEquation += "-";
+                negativeClicked = true;
+            }
+            UpdateDisplay();
+        }
+
+        private void Point_Click(object sender, RoutedEventArgs e)
+        {
+            if (dotClicked == false)
+            {
+                actualEquation += ".";
+                dotClicked = true;
+            }
+            UpdateDisplay();
+        }
+
+        private void Multiply_Click(object sender, RoutedEventArgs e)
+        {
+            OperatorClicked("*");
+            UpdateDisplay();
+        }
+
+        private void Divide_Click(object sender, RoutedEventArgs e)
+        {
+            OperatorClicked("/");
+            UpdateDisplay();
+        }
+
+        private void Minus_Click(object sender, RoutedEventArgs e)
+        {
+            OperatorClicked("-");
+            UpdateDisplay();
+        }
+
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            OperatorClicked("+");
+            UpdateDisplay();
+        }
+
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            float calculationResult = calculator.CalculateEquation(actualEquation);
+            ResulText.Text = calculationResult.ToString();
+            resultCalculated = true;
+            UpdateDisplay();
+        }
+
+        private void RightParenthesis_Click(object sender, RoutedEventArgs e)
+        {
+            OperatorClicked(")");
+            UpdateDisplay();
+        }
+
+        private void LeftParenthesis_Click(object sender, RoutedEventArgs e)
+        {
+            OperatorClicked("(");
+            UpdateDisplay();
+        }
+    }
+}
